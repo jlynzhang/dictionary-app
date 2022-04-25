4947fa9801e4495dc7ff182c4139901c528e83e7
jlynzhang <92702611+jlynzhang@users.noreply.github.com>
2022-04-25 13:12:19 -0500

    Integrate + style photos

--- src/DictionaryMain.js
+++ src/DictionaryMain.js
@@ -1,20 +1,32 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./DictionaryMain.css";
 
 export default function DictionaryMain(props) {
   const [keyword, setKeyword] = useState(props.defaultKeyword);
   const [results, setResults] = useState(null);
   const [loaded, setLoaded] = useState(false);
+  const [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionResponse);
+
+    let pexelsApiKey =
+      "563492ad6f917000010000016b6be717ae9c4c2dadec62ed53dcd14e";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -49,6 +61,7 @@ export default function DictionaryMain(props) {
         </div>
         <div className="container">
           <Results results={results} />
+          <Photos photos={photos} />
         </div>
       </div>
     );
